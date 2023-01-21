c1bdc37a0fe43e5d7e9b34b4facdd9811a52e4d7
GuilhermeBiscalchin <desenvolvendogame@gmail.com>
2023-01-21 14:59:51 -0300

    Servidor DetalhesI

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,12 +7,15 @@ import { appRoutes } from "./routes";
 const app = Fantify();
 
 //para habilitar o cors, dessa forma qualquer pode acessar a api, com o 'origin', posso definir quais os locais que pode acessar.
-app.register(cors);
+app.register(cors,{
+  origin:true,
+});
 app.register(appRoutes);
 
 app
   .listen({
     port: 3333,
+    host:'0.0.0.0',
   })
   .then(() => {
     console.log("HTTP Server running!");
